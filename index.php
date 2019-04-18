5cae3ffa7b0963dd022c75c1ca6b47895ca13cde
ec-Token <49738545+ec-Token@users.noreply.github.com>
2019-04-18 04:16:14 +0200

    first upload

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ecToken - the time of IOT payments</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+	<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+	<link rel="shortcut icon" href="favicon.ico">
+
+	<meta name="theme-color" content="#000C1C">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/reponsive.css">
+</head>
+<body>
+
+<section id="site_main">
+	
+    <div id="row1">
+    	<div id="bg_gradient">
+    		<table align="center" id="logo_main_top">
+    		<tr><td align="center"><span style="font-size: 36px; padding: 0; margin: 0;">ecToken</span></td></tr>
+    		<tr><td align="center"><img src="images/logo.svg" type="image/svg+xml" height="200" width="200" id="logo"></td></tr>
+    		<tr><td><text style="color: black; font-size: 24px; font-family: timeburnerbold;">the time of IOT payments</text></td></tr>
+    	</table>
+    	</div>
+    	
+    </div>
+
+    <div id="row2">
+    	<table align="center" id="row2_tbl" width="70%">
+    		<tr><td align="center" colspan="3" id="title_desc">- Technical -</td></tr>
+    		<tr><td colspan="3" id="desc">ecToken is the first payment system for IOT devices based on the Tron blockchain.</td></tr>
+    		<tr>
+    			<td align="center" width="33%" style="vertical-align: top;">
+    				<table>
+    					<tr><td align="center"><img src="images/lock.svg" height="64" width="64"></td></tr>
+    					<tr><td id="part_title">Very Secure</td></tr>
+    					<tr><td>The ecToken uses a very secure connection in the interactions of the microcontroller and the blockchain. The ecToken protocol uses the secure SSL procedure based on Symetric AES and Asymmetric RSA encryption.</td></tr>
+    				</table>
+    			
+    			
+    			</td>
+    			<td align="center" width="33%" style="vertical-align: top;">
+    				<table>
+    					<tr><td align="center"><img src="images/scalability.svg" height="64" width="64"></tr>
+    					<tr><td id="part_title">High Scabality</td></tr>
+    					<tr><td>Since the ecToken is based on the Tron Blockchain, the scalability is very high. The Tron Blockchain manages up to 2,000 transactions per second, faster than VISA transactions.</td></tr>
+    				</table>
+    			</td>
+    			<td align="center" width="33%" style="vertical-align: top;">
+    				<table>
+    					<tr><td align="center"><img src="images/scalability.svg" height="64" width="64"></tr>
+    					<tr><td id="part_title">High Scabality</td></tr>
+    					<tr><td>The ecToken uses a very secure connection in the interactions of the microcontroller and the blockchain. The ecToken protocol uses the secure SSL procedure based on Symetric AES and Asymmetric RSA encryption.</td></tr>
+    				</table>
+    			</td>
+    		</tr>
+    	</table>
+    </div>
+</section>
+</body>
+</html>
